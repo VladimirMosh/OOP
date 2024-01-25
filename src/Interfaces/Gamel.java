2ea4a710bd0a9c83f842e2682cffa3ee595749f0
VladimirMosh <the.uncle.mosh@gmail.com>
2024-01-25 17:32:34 +0300

    ReplacingTheGame

--- src/Interfaces/Gamel.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-import java.util.ArrayList;
-import Hero;
-
-public interface Gamel {
-    void gameStep(ArrayList<Hero> teamEnemy, ArrayList<Hero> teamAllias);
-
-}
--- src/Names.java
+++
@@ -1,3 +0,0 @@
-public enum Names {
-    BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7,
-}
